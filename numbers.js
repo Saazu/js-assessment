ef6a76332e220be9bddbdda2323a1de06de69efb
Alfred Tuotol Delle <alfred.delle@yale.edu>
2020-05-11 21:29:56 +0000

    Create numbers.js

---
+++ numbers.js
@@ -0,0 +1,19 @@
+exports = typeof window === 'undefined' ? global : window;
+
+exports.numbersAnswers = {
+  valueAtBit: function(num, bit) {
+    return 1 & (num >> (bit - 1));
+  },
+
+  base10: function(str) {
+    return parseInt(str, 2);
+  },
+
+  convertToBinary: function(num) {
+    
+  },
+
+  multiply: function(a, b) {
+
+  }
+};
